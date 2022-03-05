0dd6abfa80027112592f407feec8dc5e96b7c116
scheuclu <scheuclu@gmail.com>
2022-03-05 16:12:51 +0100

    Added first tests for SolnSquareVerifier.sol

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -2,16 +2,18 @@
 pragma solidity 0.8.12;
 
 import "./ScheucluHouseToken.sol";
+import "./SquareVerifier.sol";
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 
-contract SolnSquareVerifier is ScheucluHouseToken {
+contract SolnSquareVerifier is ScheucluHouseToken, SquareVerifier {
     // TODO define a solutions struct that can hold an index & an address
     struct Solution {
-        uint256 index;
-        address adr;
+        uint256 id;
+        address from;
+        bool isMinted;
     }
 
     // TODO define an array of the above struct
@@ -25,24 +27,34 @@ contract SolnSquareVerifier is ScheucluHouseToken {
     event SolutionAdded(bytes32 index, address account);
 
     // TODO Create a function to add the solutions to the array and emit the event
-    function addSolution(bytes32 index, address account) internal {
-        require(!submittedSolutions[index], "Solution is already submitted");
+    //function addSolution(bytes32 index, address account) internal {
+    function addSolution(Proof memory proof, uint[2] memory input) public {
 
-        solutions.push(Solution(solutions.length, account));
-        submittedSolutions[index] = true;
+        require(verifyTx(proof,input), "The submitted solution is not valid");
 
-        emit SolutionAdded(index, account);
+        //Hash the solution to make sure it is new
+        bytes32 hashedSolution = keccak256(abi.encodePacked(input));
+        require(!submittedSolutions[hashedSolution], "Solution is already submitted");
+        
+        uint256 index = solutions.length; //TODO lukas this is clearly wrong
+        solutions.push(Solution(index, msg.sender, false));
+
+        submittedSolutions[hashedSolution] = true;
+
+        emit SolutionAdded(hashedSolution, msg.sender);
     }
 
     // TODO Create a function to mint new NFT only after the solution has been verified
     //  - make sure the solution is unique (has not been used before)
     //  - make sure you handle metadata as well as tokenSuplly
-    function mint(address to) public {
-        bytes32 solutionIndex = 0;
+    // function mint(address to) public {
+    //     bytes32 solutionIndex = 0;
 
-        addSolution(solutionIndex, to);
-        super.mint(to, solutions.length);
-    }
+    //     //TODO(lukas)
+
+    //     //addSolution(solutionIndex, to);
+    //     super.mint(to, solutions.length);
+    // }
 
     // function isSolutionSubmitted (Proof memory proof, uint[2] memory inputs) public view returns (bool) {
     //     bytes32 solutionIndex = _getSolutionIndex(proof, inputs);
--- eth-contracts/test/TestSolnSquareVerifier.js
+++ eth-contracts/test/TestSolnSquareVerifier.js
@@ -1,3 +1,47 @@
 // Test if a new solution can be added for contract - SolnSquareVerifier
 
 // Test if an ERC721 token can be minted for contract - SolnSquareVerifier
+
+var SolnSquareVerifier = artifacts.require('SolnSquareVerifier');
+var verifierProof = require('../../zokrates/code/square/proof.json');
+
+const {
+    BN,
+    expectEvent,
+    expectRevert,
+} = require('@openzeppelin/test-helpers');
+
+contract('Test Verifier', accounts => {
+
+    const account_one = accounts[0];
+    const account_two = accounts[1];
+
+    describe('Verifier', function () {
+        before(async function () {
+            this.contract = await SolnSquareVerifier.new();
+
+        })
+
+        it('Test if a new solution can be added for contract', async function () {
+
+            let result = await this.contract.addSolution(
+                { a: verifierProof.proof.a, b: verifierProof.proof.b, c: verifierProof.proof.c },
+                verifierProof.inputs,
+                { from: account_one }
+            );
+            await expectEvent(result, "SolutionAdded");
+        })
+
+        it('Make sure the same solution cannot be added twice', async function () {
+
+            let result = await this.contract.addSolution(
+                { a: verifierProof.proof.a, b: verifierProof.proof.b, c: verifierProof.proof.c },
+                verifierProof.inputs,
+                { from: account_one }
+            );
+            await expectEvent(result, "SolutionAdded");
+        })
+
+
+    });
+})
